56d2b39a6b33377d05161ac7ff013785ae55edf8
kindra-saab <jatin0687.cse19@chitkara.edu.in>
2021-12-13 14:18:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React,{useState} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import Form from './Components/Form/Form'
+import Information from './Components/Information/Information';
+import axios from 'axios'
+
+function App() {
+        let count=0;
+        const [state,setState] = useState({})
+    const getDataFromServer = (city)=>{
+       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d2c7981dfed5b410991be2839ff35d7c`).then((res)=>{
+         console.log(res.data)
+         setState(res.data)
+       }, count=1)
+    }
+  return (
+    <div className="App">
+     <h2 className="text-white">Weather App</h2>
+     <Form getDataFromServer={getDataFromServer} />
+     {Object.keys(state).length===0?<div><h2 className="text-danger">Data is Not Available</h2></div>:<Information info={state} />}
+    </div>
+  );
+}
+
+export default App;
